f8cc963be12a569a92d247c6945477046b2cd51d
Albert Cid Royo <104758811+acidroyo-UMCU@users.noreply.github.com>
2024-08-28 08:18:29 +0200

    Update get_records.R

--- R/get_records.R
+++ R/get_records.R
@@ -6,8 +6,6 @@
 #' @param variable_name The name of the variable.
 #' @param window_name The name of the window.
 #' @param records_table The record table data object of interest; long format dataset with events per row, minimum expected columns are: person_id, date, value.
-#' @param start_window_date_col_name The start date column name in the scri_trimmed. Default is "start".
-#' @param end_window_date_col_name The end date column name in the scri_trimmed. Default is "end".
 #' @param only_first_date A boolean indicating whether to only use the first date. Default is FALSE.
 #' @param wide_format_input A boolean indicating whether the records table is in wide format or not. TRUE applies a melting procedure.
 #' @param start_prefix A string that defines the prefix for the start of a window. Default is "start".
@@ -20,8 +18,6 @@ get_records <- function(scri_trimmed,
                         variable_name,
                         window_name,
                         records_table, # record table of interest, minimum expected columns are: person_id, date, value
-                        start_window_date_col_name, # column name
-                        end_window_date_col_name, # column name
                         only_first_date = FALSE,
                         wide_format_input = TRUE,
                         start_prefix = "start",
@@ -59,12 +55,12 @@ get_records <- function(scri_trimmed,
     "SELECT *",
     only_first_date_part1,
     " FROM (",
-    "SELECT DISTINCT t1.person_id, t2.date, (t1.", end_window_date_col_name, " - t1.", start_window_date_col_name, ")
-                    AS  WindowLength, t1.", start_window_date_col_name, " AS START_DATE, t1.", end_window_date_col_name, " AS END_DATE",
+    "SELECT DISTINCT t1.person_id, t2.date, (t1.", end_prefix, " - t1.", start_prefix, ")
+                    AS  WindowLength, t1.", start_prefix, " AS START_DATE, t1.", end_prefix, " AS END_DATE",
     " FROM scri_trimmed_c t1",
     " INNER JOIN records_table t2",
     " ON (t1.person_id = t2.person_id
-          AND (t2.date BETWEEN t1.", start_window_date_col_name, " AND t1.", end_window_date_col_name, ") AND t1.WindowName == '", window_name, "')",
+          AND (t2.date BETWEEN t1.", start_prefix, " AND t1.", end_prefix, ") AND t1.WindowName == '", window_name, "')",
     ")",
     ")",
     only_first_date_part2
