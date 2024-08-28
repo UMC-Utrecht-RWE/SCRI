57f16522e6b8b60c18784219a0d125f5742629e1
Albert Cid Royo <104758811+acidroyo-UMCU@users.noreply.github.com>
2024-08-28 08:20:53 +0200

    Update record table metadata

--- R/RecordsTable.R
+++ R/RecordsTable.R
@@ -1,11 +1,10 @@
 #' Records Table
 #'
-#'  A synthetic record table; one or multiple row per individual
+#' A synthetic record table; one or multiple row per individual
 #'
 #' @format ## `RecordsTable`
-#' A data frame with 20 rows and 3 variables:
+#' A data table with 20 rows and 3 variables:
 #' \describe{
-#'  \describe{
 #'  \item{person_id}{identifier for unique iindividuals}
 #'  \item{date}{Date variables relating date of the record}
 #'  \item{value}{value of the record}
